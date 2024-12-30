c99fd0bf0b5ff45c6808ce7ca99e3a4b9b18db55
Dastanbekov <dastanbekovaitmyrza@gmail.com>
2024-12-30 15:38:38 +0600

    added new func

--- app/handlers/user.py
+++ app/handlers/user.py
@@ -1,12 +1,14 @@
 from aiogram.types import Message,CallbackQuery
 from aiogram import Router,F
 from aiogram.filters import Command,CommandStart
+from aiogram.fsm.context import FSMContext
 
 from ..keyboards.user_kb import main
 user = Router()
 
 @user.message(CommandStart())
-async def cmd_start(message: Message):
+async def cmd_start(message: Message,state:FSMContext):
+    state.clear()
     await message.answer(f'Hello User!', reply_markup=main)
 
 @user.callback_query(F.data == 'menu')
