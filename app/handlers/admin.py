87b84f20e31f40b34a36954ed510e6822a9ade66
Dastanbekov <dastanbekovaitmyrza@gmail.com>
2025-01-01 21:40:15 +0600

    just add text in to admin.py

--- app/handlers/admin.py
+++ app/handlers/admin.py
@@ -6,12 +6,14 @@ from ..keyboards.admin_kb import main
 
 admin = Router()
 
-ADMIN_ID = 6807370883
+ADMIN_ID = 68073708833
 @admin.message(Command('admin'))
 async def admin_cmd(message:Message):
     if ADMIN_ID == message.from_user.id:
         await message.answer(text='You entered into admin panel.',reply_markup=main)
 
-@admin.callback_query(F.data == 'chikibambonito')
+@admin.callback_query(F.data == 'statistics')
 async def chikibambonito(callback:CallbackQuery):
-    await callback.message.edit_text(text='Chikibambonito!')
+    await callback.message.edit_text(text='function to get stats; 1.amount of people registered 2. how much usages was in youtube downloader')
+
+
