6b693baab1de9ed25189b625f178f5dc9e12a310
Dastanbekov <dastanbekovaitmyrza@gmail.com>
2025-01-04 00:50:01 +0600

    text change

--- app/keyboards/admin_kb.py
+++ app/keyboards/admin_kb.py
@@ -2,6 +2,5 @@ from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
                            InlineKeyboardMarkup, InlineKeyboardButton)
 
 main = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Chikibambonito!', callback_data='chikibambonito')],
     [InlineKeyboardButton(text='Statistics',callback_data='statistics')]
 ])
